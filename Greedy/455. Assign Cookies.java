b91f018222ee0ddb0def300ff6b8c0f3ccd58449
suhanlu <suhan.lu37@gmail.com>
2023-10-31 23:36:19 -0400

    1. Greedy - LC455

---
+++ Greedy/455. Assign Cookies.java
@@ -0,0 +1,16 @@
+class Solution {
+    // greedy in terms of that we want to use the smallest cookie to feed the child with smallest greed factor
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int start = 0;
+        int count = 0;
+        for (int i = 0; i < s.length && start < g.length; i++) {
+            if (s[i] >= g[start]) {
+                start++;
+                count++;
+            }
+        }
+        return count;
+    }
+}
