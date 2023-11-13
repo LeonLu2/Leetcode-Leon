af0c7af34e9030ba15dab04d63b4ee4cec6bd908
suhanlu <suhan.lu37@gmail.com>
2023-11-13 13:18:05 -0500

    1. BFS - LC 199

---
+++ BFS/199. Binary Tree Right Side View.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> res = new LinkedList<Integer>();
+        if(root == null) return res;
+        List<TreeNode> candidates = new LinkedList<TreeNode>();
+        candidates.add(root);
+        while(!candidates.isEmpty()) {
+            List<TreeNode> temp = new LinkedList<TreeNode>();
+            res.add(candidates.get(0).val);
+            for(TreeNode curr : candidates) {
+                if(curr.right != null)
+                    temp.add(curr.right);
+                if(curr.left != null)
+                    temp.add(curr.left);
+            }
+            candidates = temp;
+        }
+        return res;
+    }
+}
