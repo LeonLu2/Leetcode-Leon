0af97d14ee1d026f15775c364c2cf890a1b7f748
suhanlu <suhan.lu37@gmail.com>
2023-11-14 23:06:50 -0500

    1. BFS - LC 116

---
+++ BFS/116. Populating Next Right Pointers in Each Node.java
@@ -0,0 +1,39 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        if(root == null) return null;
+        Queue<Node> level = new LinkedList<>(List.of(root));
+        while(level.size() != 0){
+            int length = level.size();
+            for(int i = 0; i < length; i++){
+                Node curr = level.poll();
+                if(i != length - 1) curr.next = level.peek();
+                if(curr.left != null) level.offer(curr.left);
+                if(curr.right != null) level.offer(curr.right);
+            }
+        }
+        return root;
+    }
+}
