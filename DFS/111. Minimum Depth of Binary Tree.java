b666c6fd3e488d20f6dcc88293f91fc43f63ca96
suhanlu <suhan.lu37@gmail.com>
2023-11-16 10:26:16 -0500

    1. BFS - LC 111 2. fix DFS - LC 111

--- DFS/111. Minimum Depth of Binary Tree.java
+++ DFS/111. Minimum Depth of Binary Tree.java
@@ -15,20 +15,11 @@
  */
 class Solution {
     public int minDepth(TreeNode root) {
-        if (root == null) return 0;
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-        int level = 1;
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                TreeNode curNode = queue.poll();
-                if (curNode.left == null && curNode.right == null) return level;
-                if (curNode.left != null) queue.offer(curNode.left);
-                if (curNode.right != null) queue.offer(curNode.right);
-            }
-            level++;
-        }
-        return level;
+        if(root == null) return 0;
+        if(root.left == null && root.right == null) return 1;
+        int min = Integer.MAX_VALUE;
+        if(root.left != null) min = Math.min(minDepth(root.left), min);
+        if(root.right != null) min = Math.min(minDepth(root.right), min);
+        return min + 1;
     }
 }
