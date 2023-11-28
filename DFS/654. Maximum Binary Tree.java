b87560b8f718dbeb22e6693ec8fd75dbb639ef3b
suhanlu <suhan.lu37@gmail.com>
2023-11-28 16:54:00 -0500

    1. DFS - LC 654

---
+++ DFS/654. Maximum Binary Tree.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode constructMaximumBinaryTree(int[] nums) {
+        return constructMaximumBinaryTree1(nums, 0, nums.length);
+    }
+
+    public TreeNode constructMaximumBinaryTree1(int[] nums, int leftIndex, int rightIndex) {
+        if (rightIndex - leftIndex < 1) {// no element
+            return null;
+        }
+        if (rightIndex - leftIndex == 1) {// only one element
+            return new TreeNode(nums[leftIndex]);
+        }
+        int maxIndex = leftIndex;// max index
+        int maxVal = nums[maxIndex];// max value
+        for (int i = leftIndex + 1; i < rightIndex; i++) { // find the max value and its index
+            if (nums[i] > maxVal){
+                maxVal = nums[i];
+                maxIndex = i;
+            }
+        }
+        TreeNode root = new TreeNode(maxVal);
+        // split the array into two parts according to the max index
+        root.left = constructMaximumBinaryTree1(nums, leftIndex, maxIndex);
+        root.right = constructMaximumBinaryTree1(nums, maxIndex + 1, rightIndex);
+        return root;
+    }
+}
