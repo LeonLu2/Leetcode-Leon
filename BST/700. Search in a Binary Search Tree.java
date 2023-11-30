e977dd98fd6a7e93d45f90b63f413354596dd632
suhanlu <suhan.lu37@gmail.com>
2023-11-29 20:37:08 -0500

    1. BST - LC 700

--- BST/700. Search in a Binary Search Tree.java
+++ BST/700. Search in a Binary Search Tree.java
@@ -25,3 +25,14 @@ class Solution {
         }
     }
 }
+
+// solution 2: iteration
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        while (root != null)
+            if (val < root.val) root = root.left;
+            else if (val > root.val) root = root.right;
+            else return root;
+        return null;
+    }
+}
