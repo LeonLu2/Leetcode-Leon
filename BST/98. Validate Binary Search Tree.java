748d23a0090c32e02cb5d34fd6b5c05f95b0dec0
suhanlu <suhan.lu37@gmail.com>
2023-11-29 20:42:41 -0500

    1. BST - LC 98

---
+++ BST/98. Validate Binary Search Tree.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    // recursion
+    TreeNode max;
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        // left
+        boolean left = isValidBST(root.left);
+        if (!left) {
+            return false;
+        }
+        // mid
+        if (max != null && root.val <= max.val) {
+            return false;
+        }
+        max = root;
+        // right
+        boolean right = isValidBST(root.right);
+        return right;
+    }
+}
