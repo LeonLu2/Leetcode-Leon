e4ffd88e188d55fc060d41927f7fb04fee5eedd9
suhanlu <suhan.lu37@gmail.com>
2023-12-07 10:23:09 -0500

    1. BST - LC 701

---
+++ BST/701. Insert into a Binary Search Tree.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) // reaches proper position
+            return new TreeNode(val);
+            
+        if (root.val < val){
+            root.right = insertIntoBST(root.right, val); // create right subtree
+        }else if (root.val > val){
+            root.left = insertIntoBST(root.left, val); // create left subtree
+        }
+        return root;
+    }
+}
+// solution 2
+class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) return new TreeNode(val);
+        TreeNode newRoot = root;
+        TreeNode pre = root;
+        while (root != null) {
+            pre = root;
+            if (root.val > val) {
+                root = root.left;
+            } else if (root.val < val) {
+                root = root.right;
+            } 
+        }
+        if (pre.val > val) {
+            pre.left = new TreeNode(val);
+        } else {
+            pre.right = new TreeNode(val);
+        }
+
+        return newRoot;
+    }
+}
