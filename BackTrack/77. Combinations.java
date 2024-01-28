5e18751ac7f54450502ecc8e8f01a85dc2306c1e
suhanlu <suhanl@andrew.cmu.edu>
2024-01-28 14:31:16 -0500

    1. BackTrack - LC 77

---
+++ BackTrack/77. Combinations.java
@@ -0,0 +1,25 @@
+class Solution {
+    List<List<Integer>> result = new ArrayList<>();
+    LinkedList<Integer> path = new LinkedList<>();
+    public List<List<Integer>> combine(int n, int k) {
+        combineHelper(n, k, 1);
+        return result;
+    }
+
+    /**
+     * each time we add a number to the path, we need to record the startIndex
+     * @param startIndex the number we start to add to the path
+     */
+    private void combineHelper(int n, int k, int startIndex){
+        // end condition
+        if (path.size() == k){
+            result.add(new ArrayList<>(path));
+            return;
+        }
+        for (int i = startIndex; i <= n - (k - path.size()) + 1; i++){
+            path.add(i);
+            combineHelper(n, k, i + 1);
+            path.removeLast();
+        }
+    }
+}
