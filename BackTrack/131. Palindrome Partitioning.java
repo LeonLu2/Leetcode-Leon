d8943d4c5c38e8150f01496482a4ddd795f5f0e2
suhanlu <suhanl@andrew.cmu.edu>
2024-02-11 21:59:04 -0500

    1. BackTrack - LC 131

---
+++ BackTrack/131. Palindrome Partitioning.java
@@ -0,0 +1,38 @@
+class Solution {
+    List<List<String>> lists = new ArrayList<>();
+    Deque<String> deque = new LinkedList<>();
+
+    public List<List<String>> partition(String s) {
+        backTracking(s, 0);
+        return lists;
+    }
+
+    private void backTracking(String s, int startIndex) {
+        // if the start index is equal to the length of the string, then we have found a partition
+        if (startIndex >= s.length()) {
+            lists.add(new ArrayList(deque));
+            return;
+        }
+        for (int i = startIndex; i < s.length(); i++) {
+            // if the substring from start index to i is a palindrome, then add it to the deque
+            if (isPalindrome(s, startIndex, i)) {
+                String str = s.substring(startIndex, i + 1);
+                deque.addLast(str);
+            } else {
+                continue;
+            }
+            // continue to find the next palindrome substring
+            backTracking(s, i + 1);
+            deque.removeLast();
+        }
+    }
+
+    private boolean isPalindrome(String s, int startIndex, int end) {
+        for (int i = startIndex, j = end; i < j; i++, j--) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
